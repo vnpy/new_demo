1f5396696411f3fc63824ee6a85e5865c8b32c3c
vn.py <xiaoyou.chen@foxmail.com>
2022-08-27 16:03:12 +0800

    [Mod] 第二段：实现UI数据输出

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
-# from vnpy_ctp.api import MdApi
+from PySide6 import QtWidgets
+
 from vnpy_tts.api import MdApi
 
 
 class CtpMdApi(MdApi):
 
-    def __init__(self) -> None:
+    def __init__(self, monitor) -> None:
         super().__init__()
 
+        self.monitor = monitor
+
     def onFrontConnected(self):
-        print("服务器连接成功")
+        self.monitor.append("服务器连接成功")
 
         ctp_req = {
             "UserID": "000300",
@@ -18,27 +21,33 @@ class CtpMdApi(MdApi):
         self.reqUserLogin(ctp_req, 1)
 
     def onFrontDisconnected(self, reason):
-        print("服务器连接断开", reason)
+        self.monitor.append("服务器连接断开", reason)
 
     def onRspUserLogin(self, data, error, reqid, last):
         if not error["ErrorID"]:
-            print("行情服务器登录成功")
+            self.monitor.append("行情服务器登录成功")
 
             self.subscribeMarketData("rb2301")
         else:
-            print("行情服务器登录失败", error)
+            self.monitor.append("行情服务器登录失败", error)
 
     def onRtnDepthMarketData(self, data):
-        print(data)
+        self.monitor.append(str(data))
 
 
 def main():
-    api = CtpMdApi()
+    app = QtWidgets.QApplication()
+
+    monitor = QtWidgets.QTextEdit()
+    monitor.show()
+
+    api = CtpMdApi(monitor)
     api.createFtdcMdApi(".")
     # api.registerFront("tcp://180.168.146.187:10130")  # ctp
     api.registerFront("tcp://122.51.136.165:20004")
     api.init()
-    input()
+
+    app.exec()
 
 
 if __name__ == "__main__":
